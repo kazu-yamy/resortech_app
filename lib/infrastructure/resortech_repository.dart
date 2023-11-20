2392ca0dd51d84fe25578d84bf70d267fbb10f70
kazu-yamy <kazuki.aishima.dev@gmail.com>
2023-11-20 10:31:08 +0900

    update ipaddress

--- lib/infrastructure/resortech_repository.dart
+++ lib/infrastructure/resortech_repository.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 class ResortechRepository {
   static postImage(String imagePath) async {
-    final url = Uri.parse('http://192.168.10.2:8000/api/v1/predict');
+    final url = Uri.parse('https://xxx.xxx.xxx.xxx');
     final request = http.MultipartRequest('POST', url);
 
     final file = await http.MultipartFile.fromPath('image', imagePath);
